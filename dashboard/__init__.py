c0bc5e45058187f9cbd9c8f16e0e6e678e0d484c
Paul Chote <p.chote@warwick.ac.uk>
2018-11-08 21:44:19 +0000

    Add wasp computer room camera.

--- dashboard/__init__.py
+++ dashboard/__init__.py
@@ -249,10 +249,15 @@ def nites_dome():
     dashboard_mode = __parse_dashboard_mode()
     return render_template('nites/dome.html', user_account=get_user_account(), dashboard_mode=dashboard_mode)
 
-@app.route('/wasp/dome/')
-def wasp_dome():
+@app.route('/wasp/dome1/')
+def wasp_dome1():
     dashboard_mode = __parse_dashboard_mode()
-    return render_template('wasp/dome.html', user_account=get_user_account(), dashboard_mode=dashboard_mode)
+    return render_template('wasp/dome1.html', user_account=get_user_account(), dashboard_mode=dashboard_mode)
+
+@app.route('/wasp/dome2/')
+def wasp_dome2():
+    dashboard_mode = __parse_dashboard_mode()
+    return render_template('wasp/dome2.html', user_account=get_user_account(), dashboard_mode=dashboard_mode)
 
 @app.route('/wasp/live/')
 def wasp_live():
