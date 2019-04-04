56ddd6b353dd1f8125e550eaa4dbe890a4438bfe
Paul Chote <p.chote@warwick.ac.uk>
2019-04-04 10:11:58 +0100

    Migrate W1m to observatory environment.

--- static/common-dashboard.js
+++ static/common-dashboard.js
@@ -48,3 +48,73 @@ function pollDashboard(url) {
   window.setTimeout(function() { pollDashboard(url); }, 10000);
 }
 
+// Environment generators
+function fieldLimitsColor(field, value) {
+  if (!('limits' in field))
+    return null;
+
+  if (field['disabled'])
+    return 'text-info';
+
+  if (value < field['limits'][0] || value > field['limits'][1])
+    return 'text-danger';
+
+  if ('warn_limits' in field && (value < field['warn_limits'][0] || value > field['warn_limits'][1]))
+    return 'text-warning';
+
+  return 'text-success';
+}
+
+function envLatestMinMax(row, cell, data) {
+  if ('latest' in data) {
+    var display = data['latest'].toFixed(1);
+    var units = row.data('units');
+    if (units)
+      display += units;
+    cell.html(display);
+    cell.addClass(fieldLimitsColor(data, data['latest']));
+  } else {
+    cell.html('NO DATA');
+    cell.addClass('text-danger');
+  }
+
+  if ('max' in data && 'min' in data) {
+    var maxValue = $('<span>');
+    maxValue.html(data['max'].toFixed(1));
+    maxValue.addClass(fieldLimitsColor(data, data['max']));
+
+    var minValue = $('<span>');
+    minValue.html(data['min'].toFixed(1));
+    minValue.addClass(fieldLimitsColor(data, data['min']));
+
+    var maxMinValue = $('<span class="pull-right data-minmax">');
+    maxMinValue.append(maxValue);
+    maxMinValue.append('<br>');
+    maxMinValue.append(minValue);
+    cell.append(maxMinValue);
+  }
+}
+
+function diskSpaceGB(row, cell, data) {
+  if ('latest' in data) {
+    var display = +(data['latest'] / 1073741824).toFixed(1);
+    var units = row.data('units');
+    if (units)
+      display += units;
+    cell.html(display);
+    cell.addClass(fieldLimitsColor(data, data['latest']));
+  }
+}
+
+function seeingIfAvailable(row, cell, data) {
+  if ('latest' in data && data['latest'] > 0) {
+    var display = data['latest'].toFixed(2);
+    var units = row.data('units');
+    if (units)
+      display += units;
+    cell.html(display);
+  } else {
+    cell.html('NO DATA');
+    cell.addClass('text-danger');
+  }
+}
